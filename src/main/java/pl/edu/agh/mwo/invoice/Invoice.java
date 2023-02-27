5efb2817fc37e0f75669a1af8f284537e47cb189
Jaroslaw Oskroba <jaroslawoskroba@gmail.com>
2023-02-27 19:39:23 +0100

    covered 13 of 14 tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,27 +9,51 @@ import java.util.Map;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+
+//    Collection<Product> products = new ArrayList<>();
+    Map<Product, Integer> productIntegerMap = new HashMap<Product, Integer>();
 
     public void addProduct(Product product) {
-        products.add(product);
+        if (product == null){
+            throw new IllegalArgumentException();
+        }
+        productIntegerMap.put(product,1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        Map<Product, Integer> productIntegerMap = new HashMap<Product, Integer>();
+        if(quantity.compareTo(0) <= 0){
+            throw new IllegalArgumentException();
+        }
         productIntegerMap.put(product,quantity);
     }
     public BigDecimal getSubtotal() {
+            BigDecimal subtotal = BigDecimal.ZERO;
 
-        return BigDecimal.ZERO;
+            for(Map.Entry<Product, Integer> entry : productIntegerMap.entrySet()){
+                subtotal = subtotal.add(entry.getKey().getPrice().multiply(BigDecimal.valueOf(entry.getValue())));
+            }
+
+            return subtotal;
+
     }
 
     public BigDecimal getTax() {
-        return null;
+        BigDecimal fullTax = BigDecimal.ZERO;
+
+        for(Map.Entry<Product, Integer> entry : productIntegerMap.entrySet()){
+            fullTax = fullTax.add(entry.getKey().getTaxPercent().multiply(entry.getKey().getPrice()));
+        }
+
+        return fullTax;
     }
 
     public BigDecimal getTotal() {
+        BigDecimal total = BigDecimal.ZERO;
 
-        return BigDecimal.ZERO;
+        for(Map.Entry<Product, Integer> entry : productIntegerMap.entrySet()){
+            total = total.add(entry.getKey().getPriceWithTax().multiply(BigDecimal.valueOf(entry.getValue())));
+        }
+
+        return total;
     }
 }
